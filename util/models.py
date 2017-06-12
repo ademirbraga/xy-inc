cd080f10f3d34e4510d2789f68d395998e427465
Ademir Braga <ademirbraga100@yahoo.com.br>
2017-06-11 22:20:17 -0300

    removendo py

--- util/models.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-
-from django.db.models import Model,DateTimeField
-
-class Padrao(Model):
-    class Meta:
-        abstract                    = True
-    data_cadastro                   = DateTimeField(verbose_name=u'Data de Cadastro',auto_now_add=True)
